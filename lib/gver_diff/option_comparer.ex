b21699c4c24d16b261121dbe0a5c1d690913f364
ya310 <mentol310@gmail.com>
2019-12-04 23:13:55 +0900

    #6 Add: Comparer considering base type

--- lib/gver_diff/option_comparer.ex
+++ lib/gver_diff/option_comparer.ex
@@ -1,15 +1,26 @@
 defmodule GverDiff.OptionComparer do
-  def compare?(%{:base => base, :target => target}, nil), do: base < target
+  def compare?(%{:base => base, :target => target}, nil) do
+    cond do
+      get_type(base) === get_type(target) -> base < target
+      true -> false
+    end
+  end
 
   def compare?(%{:base => base, :target => target}, operator) do
-    case check_operator(operator) do
-      {:equal} -> base === target
-      {:notEqual} -> base !== target
-      {:greaterThan} -> base > target
-      {:lessThan} -> base < target
-      {:greaterThanOrEqual} -> base >= target
-      {:lessThanOrEqual} -> base <= target
-      {:error} -> false
+    cond do
+      get_type(base) === get_type(target) ->
+        case check_operator(operator) do
+          {:equal} -> base === target
+          {:notEqual} -> base !== target
+          {:greaterThan} -> base > target
+          {:lessThan} -> base < target
+          {:greaterThanOrEqual} -> base >= target
+          {:lessThanOrEqual} -> base <= target
+          {:error} -> false
+        end
+
+      true ->
+        false
     end
   end
 
@@ -22,6 +33,33 @@ defmodule GverDiff.OptionComparer do
       operator == ">=" or operator == "ge" -> {:greaterThanOrEqual}
       operator == "<=" or operator == "le" -> {:lessThanOrEqual}
       true -> {:error}
+    end
+  end
+
+  defp get_type(x) do
+    if is_date(x) do
+      {:date}
+    end
+
+    cond do
+      is_integer(x) -> {:number}
+      is_boolean(x) -> {:boolean}
+      is_float({x}) -> {:float}
+      is_binary(x) -> {:string}
+      true -> {:error}
+    end
+  end
+
+  defp is_date(x) do
+    cond do
+      is_map(x) ->
+        Map.has_key?(x, :__struct__)
+        |> if do
+          x.__struct__ === NaiveDateTime
+        end
+
+      true ->
+        false
     end
   end
 end
--- test/gver_diff/option_comparer_test.exs
+++ test/gver_diff/option_comparer_test.exs
@@ -3,9 +3,34 @@ defmodule OptionComparerTest do
 
   compareIntegerValues = [
     # default operator <
+    # integer
     [:step1, %{:expect => true, :base => 3, :target => 4}],
     [:step2, %{:expect => false, :base => 4, :target => 4}],
-    [:step3, %{:expect => false, :base => 5, :target => 4}]
+    [:step3, %{:expect => false, :base => 5, :target => 4}],
+    # sting
+    [:step4, %{:expect => true, :base => "a", :target => "b"}],
+    [:step5, %{:expect => false, :base => "b", :target => "b"}],
+    [:step6, %{:expect => false, :base => "c", :target => "b"}],
+    # float
+    [:step7, %{:expect => true, :base => 3.1, :target => 3.11}],
+    [:step8, %{:expect => false, :base => 4.123, :target => 4.123}],
+    [:step9, %{:expect => false, :base => 5.234, :target => 4.222}],
+    # date
+    [
+      :step10,
+      %{:expect => true, :base => ~N[2019-11-11 11:11:11], :target => ~N[2019-11-11 22:22:22]}
+    ],
+    [
+      :step11,
+      %{:expect => false, :base => ~N[2019-11-11 11:11:11], :target => ~N[2019-11-11 11:11:11]}
+    ],
+    [
+      :step12,
+      %{:expect => false, :base => ~N[2019-11-11 11:11:11], :target => ~N[2019-11-09 11:11:11]}
+    ],
+    # different type
+    [:step13, %{:expect => false, :base => 3, :target => "4"}],
+    [:step14, %{:expect => false, :base => 3, :target => 3.11}]
   ]
 
   for [label, %{:expect => expect} = values] <- compareIntegerValues do
@@ -29,13 +54,10 @@ defmodule OptionComparerTest do
     # not equal
     [:step7, %{:expect => true, :base => 2, :target => 4, :operator => "!="}],
     [:step8, %{:expect => false, :base => 4, :target => 4, :operator => "!="}],
-    [:step9, %{:expect => true, :base => "4", :target => 4, :operator => "!="}],
     [:step10, %{:expect => true, :base => 2, :target => 4, :operator => "<>"}],
     [:step11, %{:expect => false, :base => 4, :target => 4, :operator => "<>"}],
-    [:step12, %{:expect => true, :base => "4", :target => 4, :operator => "<>"}],
     [:step13, %{:expect => true, :base => 2, :target => 4, :operator => "ne"}],
     [:step14, %{:expect => false, :base => 4, :target => 4, :operator => "ne"}],
-    [:step15, %{:expect => true, :base => "4", :target => 4, :operator => "ne"}],
     # greater than
     [:step16, %{:expect => true, :base => 5, :target => 4, :operator => ">"}],
     [:step17, %{:expect => false, :base => 4, :target => 4, :operator => ">"}],
